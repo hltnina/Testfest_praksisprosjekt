ee2f8bbaeac3ce3912c776ffd58defe1fc4088d0
Tringak1 <tringakongjeli@hotmail.com>
2025-10-23 12:19:17 +0200

    Added project files and .gitignore

---
+++ Backend/Backend/Controllers/IssuesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    public class IssuesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
